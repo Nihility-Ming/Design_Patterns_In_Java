715d0511aad376f90f56d165e5e5eaba1e71f90d
Nihility-Ming <Nihility-Ming@users.noreply.github.com>
2024-08-20 22:17:55 +0800

    update

--- src/main/java/com/miaoge/AbstractFactory/Main.java
+++ src/main/java/com/miaoge/AbstractFactory/Main.java
@@ -6,16 +6,11 @@ import com.miaoge.AbstractFactory.product.ship.Ship;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Factory factory = Factory.getInstance();
-            Car car = factory.createCar();
-            Ship ship = factory.createShip();
+        Factory factory = Factory.getInstance();
+        Car car = factory.createCar();
+        Ship ship = factory.createShip();
 
-            System.out.println(car);
-            System.out.println(ship);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        System.out.println(car);
+        System.out.println(ship);
     }
 }
